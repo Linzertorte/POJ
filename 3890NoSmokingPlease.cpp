295e04d12ef27e3b3592f94d773caa5b8de3fe51
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-01-28 21:55:03 -0500

    modified rubost dinic

--- 3890NoSmokingPlease.cpp
+++ 3890NoSmokingPlease.cpp
@@ -4,64 +4,75 @@
 using namespace std;
 #define N 1000010
 #define E N<<2
-#define typec int // type of cost
-const typec inf = 0x3f3f3f3f; // max of cost
-struct edge { int x, y, nxt; typec c; } bf[E];
-int ne, head[N], cur[N], ps[N], dep[N];
-void addedge(int x, int y, typec c)
+
+
+struct edge{
+    int x,y,nxt,c;
+} elist[E];
+const int inf = INT_MAX;
+//init  ne=0, head[] -1
+int ne,head[N],cur[N],ps[N],dep[N];
+void addedge(int x,int y,int c)
 {
-    bf[ne].x = x; bf[ne].y = y; bf[ne].c = c;
-    bf[ne].nxt = head[x]; head[x] = ne++;
-    bf[ne].x = y; bf[ne].y = x; bf[ne].c = c;
-    bf[ne].nxt = head[y]; head[y] = ne++;
+    elist[ne].x = x, elist[ne].y = y, elist[ne].c = c;
+    elist[ne].nxt = head[x], head[x] = ne++;
+    elist[ne].x = y, elist[ne].y = x, elist[ne].c = c; // 0 if edge is directed
+    elist[ne].nxt = head[y], head[y] = ne++;
 }
-long long flow(int n, int s, int t)
+
+int flow(int n,int s,int t)
 {
-    typec tr;
-    long long res = 0;
-    int i, j, k, f, r, top;
-    while (1) {
-        memset(dep, -1, n * sizeof(int));
-        for (f = dep[ps[0] = s] = 0, r = 1; f != r;)
-            for(i=ps[f++],j=head[i];j;j=bf[j].nxt)
-            {
-                if(bf[j].c && -1==dep[k=bf[j].y]){
-                    dep[k]=dep[i]+1;ps[r++]=k;
-                    if(k==t) {f=r;break;}
+    int tr;
+    int res = 0;
+    int i,j,k,f,r,top;
+    while(1)
+    {
+        memset(dep,-1,n*sizeof(int));
+        ps[0]=s, dep[s]=0,f=0,r=1;
+        while(f!=r)
+        {
+            i=ps[f++];
+            // bfs to get the level subgraph
+            for(j=head[i];j!=-1;j=elist[j].nxt)
+                if(elist[j].c && -1 == dep[k=elist[j].y])
+                {
+                    dep[k] = dep[i]+1, ps[r++]=k;
+                    if(k==t){
+                        f=r;
+                        break;
+                    }
                 }
-            }
+        }
         if(-1 == dep[t]) break;
-        memcpy(cur, head, n * sizeof(int));
-        for(i=s,top=0;;){
-            if(i==t) {
-                for(k=0,tr=inf;k<top;++k)
-                    if(bf[ps[k]].c<tr) tr=bf[ps[f=k]].c;
-                for(k=0;k<top;++k)
-                    bf[ps[k]].c -= tr, bf[ps[k]^1].c+=tr;
-                res += tr; i=bf[ps[top=f]].x;
+        memcpy(cur,head,n*sizeof(int));
+        i=s,top=0;
+        while(1)
+        {
+            if(i==t){
+                for(k=0,tr=inf;k<top;k++)
+                    if(elist[ps[k]].c<tr) tr=elist[ps[f=k]].c;
+                for(k=0;k<top;k++)
+                    elist[ps[k]].c -= tr, elist[ps[k]^1].c += tr;
+                res += tr, top=f, i=elist[ps[top]].x;
             }
-            for(j=cur[i];cur[i];j=cur[i]=bf[cur[i]].nxt)
-                if(bf[j].c && dep[i]+1 == dep[bf[j].y]) break;
-            if(cur[i]){
-                ps[top++]=cur[i];
-                i=bf[cur[i]].y;
-            }else {
+            for(j=cur[i];cur[i]!=-1;j=cur[i]=elist[cur[i]].nxt)
+                if(elist[j].c && dep[i]+1 == dep[elist[j].y]) break;
+            if(j!=-1) ps[top++]=cur[i],i=elist[j].y;
+            else{
                 if(0==top) break;
-                dep[i]=-1;i=bf[ps[--top]].x;
+                dep[i]=-1, top--, i=elist[ps[top]].x;
             }
-            
         }
     }
     return res;
 }
-
 int main()
 {
     int t;
     scanf("%d",&t);
     while(t--){
-        ne=2;
-        memset(head,0,sizeof(head));
+        ne=0;
+        memset(head,-1,sizeof(head));
         int r;int c;
         int r0,c0,r1,c1;
         scanf("%d %d",&r,&c);
@@ -84,7 +95,7 @@ int main()
             }
         
         
-        printf("%lld\n",flow(r*c,s,tt));
+        printf("%d\n",flow(r*c,s,tt));
         
     }
     return 0;
