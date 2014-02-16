c42f6ce7b1fc21d1e637980e591c3372e45a2404
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-02-15 23:23:33 -0500

    Euler path

---
+++ 2230Watchcow.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int N,M;
+struct E{
+    int v,next;
+}elist[100000];
+int head[10001];
+int u,v;
+int cnt,top;
+int path[100010];
+void addedge(int u,int v)
+{
+    elist[cnt].v=v;
+    elist[cnt].next=head[u];
+    head[u]=cnt++;
+}
+int walked[100000];
+void euler(int u)
+{
+    for(int i=head[u];i!=-1;i=elist[i].next){
+        if(walked[i]) continue;
+        walked[i]=1;
+        euler(elist[i].v);
+        path[top++]=u;
+    }
+}
+int main()
+{
+    scanf("%d%d",&N,&M);
+    memset(head,-1,sizeof(head));
+    for(int i=0;i<M;i++)
+    {
+        scanf("%d%d",&u,&v);
+        addedge(u,v);
+        addedge(v,u);
+    }
+    euler(1);
+    while(--top>=0) printf("%d\n",path[top]);
+    printf("1\n");
+    return 0;
+}
