99262b1a7ef209fa77c92ee9d733c43e80328f4a
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-08-13 23:20:15 -0400

    Update 3752字母旋转游戏.cpp

--- 3752字母旋转游戏.cpp
+++ 3752字母旋转游戏.cpp
@@ -10,23 +10,18 @@
 using namespace std;
 int N,M;
 int get_total(int m){
-    int X = M-2*(m-1);
-    int Y = N-2*(m-1);
-    int total = 2*(X+Y)-4;
-    return total;
+    if(m==0) return 0;
+    else return 2*m*(M+N)+4*m - 8*m*(m+1)/2;
 }
 int get(int i,int j){
     int m = min(i,j);
     m = min(m,M+1-i);
     m = min(m,N+1-j);
-    int off = 0;
-    for(int k=1;k<m;k++)
-        off += get_total(k);
-    int total = get_total(m);
+    int off = get_total(m-1);
     int X = M-2*(m-1), Y = N-2*(m-1);
     int dx = i-m, dy = j-m;
     if(dx==0) return off+dy+1;
-    if(dy==0) return off+total-dx+1;
+    if(dy==0) return off+2*(X+Y)-4-dx+1;
     if(dy==Y-1) return off+Y + dx;
     return off+Y + Y - dy + X-2;
     
@@ -36,7 +31,6 @@ int main(){
     for(int i=1;i<=M;i++){
         for(int j=1;j<=N;j++){
             printf("   %c",(get(i,j)-1)%26+'A');
-           
         }
         cout<<endl;
     }
