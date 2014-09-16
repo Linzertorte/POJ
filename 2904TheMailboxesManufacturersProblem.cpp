5b925b0451f5b51bdceb3196fa91cc4f9d353126
Zhaoliang Liu <Linzertorte@users.noreply.github.com>
2014-09-16 00:07:32 -0700

    Update 2904TheMailboxesManufacturersProblem.cpp

--- 2904TheMailboxesManufacturersProblem.cpp
+++ 2904TheMailboxesManufacturersProblem.cpp
@@ -12,8 +12,12 @@ int f(int k,int s,int e){
     if(s==e) return dp[k][s][e] = s;
     if(k==1) return dp[k][s][e] = (s+e)*(e-s+1)/2;
     int m1,m2,m,v = INT_MAX/2;
-    for(int t=s+1;t<e;t++){
-        m1 = f(k-1,s,t-1);
+    for(int t=s;t<=e;t++){
+        if(t==s) m1 = 0;
+        else
+            m1 = f(k-1,s,t-1);
+        if(t==e) m2 = 0;
+        else
         m2 = f(k,t+1,e);
         m = max(m1,m2);
         v = min(v,m+t);
